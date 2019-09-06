de1cc214d264d64e854119d68e4b7db6ef25eb5f
Alek Hartzog <ahartzog@gmail.com>
2019-09-06 13:53:05 -0400

    Purge junk stores

--- src/lib/actions/actionsEnum.ts
+++
@@ -1,5 +0,0 @@
-enum ActionsEnum {
-  add_article = "ADD_ARTICLE"
-}
-
-export default ActionsEnum;
--- src/lib/actions/articleActions.ts
+++
@@ -1,5 +0,0 @@
-import ActionsEnum from "./actionsEnum";
-
-export function addArticle(payload: any) {
-  return { type: ActionsEnum.add_article, payload };
-}
